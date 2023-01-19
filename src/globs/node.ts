3ebfe1bf1e521ea94620a59e70d325081919e1a6
C. Lewis <ctj.lewis@icloud.com>
2023-01-19 16:09:36 -0600

    chore: first draft

--- src/globs/node.ts
+++ src/globs/node.ts
@@ -1,25 +1,4 @@
 import { Configuration, OpenAIApi } from "openai";
-// import Stripe from "stripe";
-
-// const { STRIPE_SECRET_KEY } = process.env;
-
-// if (!STRIPE_SECRET_KEY) {
-//   throw new Error("No Stripe secret key provided.");
-// }
-
-// export const STRIPE = new Stripe(
-//   STRIPE_SECRET_KEY,
-//   {
-//     apiVersion: "2022-08-01",
-//   }
-// );
-
-// if (!process.env.AUTH0_SECRET) {
-//   throw new Error("No Auth0 secret provided.");
-// }
-
-// export const { AUTH0_SECRET } = process.env;
-
 
 /**
  * OpenAI config and client.
--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -4,47 +4,18 @@ import { pipeline } from "stream/promises";
 import { OPENAI } from "../../globs/node";
 
 import type { NextApiRequest, NextApiResponse } from "next";
+import { createCompletion } from "../../lib/createCompletionStream";
 
 export default async function test(_: NextApiRequest, res: NextApiResponse) {
-  // res.setHeader("Content-Type", "application/octet-stream");
-
-  const textStream = new Stream();
-  textStream.pipe(process.stdout);
-  textStream.pipe(res);
-
-  const completion = await OPENAI.createCompletion({
+  const stream = await createCompletion({
     model: "text-davinci-003",
     prompt: "Hi, this is just a test.\n\n",
     max_tokens: 200,
-    stream: true,
-  }, {
-    responseType: "stream",
   });
 
-  for await (const bytes of (completion.data as any)) {
-    let string = bytes.toString("utf8");
-    if (!string.startsWith("data:")) {
-      break;
-    }
+  stream.pipe(process.stdout);
+  stream.pipe(res);
 
-    string = string.slice("data: ".length);
-    if (!string.startsWith("[DONE]")) {
-      try {
-        const message = JSON.parse(string);
-        const { text } = message.choices[0];
-        textStream.emit("data", text);
-      } catch (error) {
-        console.log({ string, error });
-      }
-    }
-  }
-
-  textStream.emit("end");
-  console.log("\n---\n");
+  // eslint-disable-next-line no-console
+  stream.on("end", () => console.log());
 }
-
-// export const config = {
-//   api: {
-//     bodyParser: false,
-//   },
-// };
