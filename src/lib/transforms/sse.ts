a52338451e7d6fd5c161b8a1cde4a371688d71b2
C. Lewis <ctj.lewis@icloud.com>
2023-01-27 21:11:56 -0600

    chore: please work

--- src/lib/transforms/sse.ts
+++ src/lib/transforms/sse.ts
@@ -1,6 +1,5 @@
+import { createParser } from "eventsource-parser";
 import { Transform } from ".";
-
-const SSE_DATA_PREFIX = "data: ";
 
 /**
  * Yields Server Side Event data from an OpenAI stream.
@@ -9,22 +8,13 @@ export const SSEParser: Transform = async function* (chunk) {
   const ENCODER = new TextEncoder();
   const DECODER = new TextDecoder();
 
-  const string = DECODER.decode(chunk);
-  const sseChunks = string.split("\n\n").filter(Boolean);
-  // eslint-disable-next-line no-console
-  console.log({ sseChunks });
+  yield new Promise<Uint8Array>((resolve) => {
+    const parser = createParser((event) => {
+      if ("data" in event) {
+        resolve(ENCODER.encode(event.data));
+      }
+    });
 
-  for (const sseChunk of sseChunks) {
-    if (!sseChunk) continue;
-    if (!sseChunk.startsWith(SSE_DATA_PREFIX)) continue;
-
-    const data = sseChunk.slice(SSE_DATA_PREFIX.length);
-    // eslint-disable-next-line no-console
-    console.log({ data });
-    if (data === "[DONE]") {
-      return;
-    }
-
-    yield ENCODER.encode(data);
-  }
+    parser.feed(DECODER.decode(chunk));
+  });
 };
