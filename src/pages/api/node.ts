fef5fba5b522175cab8b85fb553c05c2fe1f2c4c
C. Lewis <ctj.lewis@icloud.com>
2023-04-08 16:12:22 -0500

    chore: `openai-streams/node` demo
    https://github.com/gptlabs/openai-streams/issues/18

---
+++ src/pages/api/node.ts
@@ -0,0 +1,25 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { OpenAI } from "openai-streams/node";
+
+export default async function demo(req: NextApiRequest, res: NextApiResponse) {
+  const { name } = JSON.parse(req.body);
+  if (!name) {
+    return res.status(400).send("Did not include `name` parameter");
+  }
+
+  const completionsStream = await OpenAI(
+    // "edits",
+    "completions",
+    {
+      // model: "text-davinci-edit-001",
+      // input: "What day of the wek is it?",
+      // instruction: "Fix the spelling mistakes",
+      model: "text-davinci-003",
+      prompt: `Write a nice two-sentence paragraph about a person named ${name}.\n\n`,
+      temperature: 1,
+      max_tokens: 100,
+    },
+  );
+
+  return res.send(completionsStream);
+}
---
+++ src/pages/node.tsx
@@ -0,0 +1,66 @@
+/* eslint-disable max-len */
+import Head from "next/head";
+import { useEffect, useRef, useState } from "react";
+import { StreamingText } from "../components/StreamingText";
+import { useTextBuffer } from "../hooks";
+
+export default function Home() {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const [data, setData] = useState({ name: "John" });
+  const { buffer, done, refresh, cancel } = useTextBuffer({
+    url: "/api/node",
+    throttle: 100,
+    method: "POST",
+    data,
+  });
+
+  // eslint-disable-next-line no-console
+  console.log({ buffer, done });
+
+  useEffect(
+    () => {
+      const name = nameRef.current?.value;
+      if (name) setData((data) => ({ ...data, name }));
+    },
+    []
+  );
+
+  const setName = () => {
+    const name = nameRef?.current?.value;
+    if (name) {
+      setData((data) => ({ ...data, name }));
+      refresh();
+    }
+  };
+
+  return (
+    <>
+      <Head>
+        <title>OpenAI Completion Stream</title>
+      </Head>
+
+      <main>
+        <div>
+          <label>Name</label>
+          <input
+            ref={nameRef}
+            defaultValue={data.name}
+            placeholder="Type a name here..."
+          />
+        </div>
+
+        <div className="w-full max-w-md p-4 rounded-lg border-solid border-2 border-gray-400">
+          {/* <StreamingTextURL url="/api/demo" fade={600} throttle={100} data={data} /> */}
+          <StreamingText buffer={buffer} />
+        </div>
+
+        <div className="flex flex-row">
+          <button className="w-full" onClick={setName}>
+            Refresh
+          </button>
+          <button className="w-full" onClick={cancel}>Cancel</button>
+        </div>
+      </main>
+    </>
+  );
+}
